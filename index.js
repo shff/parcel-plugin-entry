4c1f361f1da7246200358d7cb3236906df9968d6
Silvio Henrique Ferreira <shferreira@me.com>
2020-03-26 05:04:40 +0000

    Add code

---
+++ index.js
@@ -0,0 +1,46 @@
+const { writeFileSync } = require("fs");
+const { extname, resolve, relative } = require("path");
+const { load } = require("parcel-bundler/lib/utils/config");
+
+const entryHTML = (entry, info = {}) => `<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>${info.title || info.name || ""}</title>
+  ${
+    info.description
+      ? `<meta name="description" content="${info.description || ""}">`
+      : ""
+  }
+</head>
+<body>
+  <noscript>
+    We're sorry but ${
+      info.title || info.name || ""
+    } doesn't work properly without JavaScript enabled. Please enable it to continue.
+  </noscript>
+  <div id="app"></div>
+  <script src="${entry}"></script>
+</body>
+</html>
+`;
+
+module.exports = bundler => {
+  bundler.entryFiles.forEach(async (entry, index) => {
+    const extension = extname(entry);
+    const parser = bundler.parser.extensions[extension];
+
+    if (parser !== "./assets/JSAsset") return;
+
+    const config = await load(entry, ['package.json']);
+    const jsPath = relative(bundler.options.cacheDir, entry);
+    const htmlPath = resolve(bundler.options.cacheDir, "index.html");
+    const html = entryHTML(jsPath, config);
+    writeFileSync(htmlPath, html);
+
+    bundler.entryFiles[index] = htmlPath;
+    bundler.options.rootDir = bundler.options.cacheDir;
+  });
+};
